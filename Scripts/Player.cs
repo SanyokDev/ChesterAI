f23ca9b1655db0754034dd0704f06f8e094c3e07
TheWinterDev <alessandronikolaev.business@gmail.com>
2021-06-22 15:51:14 +0100

    Initial full project commit

---
+++ Scripts/Player.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D {
+    //Variables
+    [Export]
+    public int moveSpd = 250;
+    
+    //Update
+    public override void _PhysicsProcess(float delta) {
+        Vector2 motion = new Vector2();
+        motion.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        motion.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+
+        MoveAndCollide(motion.Normalized() * moveSpd * delta);
+    }
+}
